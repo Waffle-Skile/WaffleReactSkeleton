287e157b6b6ba8c1aba2905548e81264132db84d
Skile <imleejm@gmail.com>
2018-11-23 00:20:51 +0900

    Make some skeleton code blocks

---
+++ src/HobulhoApp.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class HobulhoApp extends Component {
+  render() {
+    return (
+      <div>
+        WRITE SOMETHING HERE!!
+      </div>
+    );
+  }
+}
+
+export default HobulhoApp;
